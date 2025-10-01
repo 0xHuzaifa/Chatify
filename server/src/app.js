f0e163889dcec2a40250008d6e5b5d128920fcb5
0xhuzaifa <huzaifa.rb00@gmail.com>
2025-10-01 22:23:54 +0500

    setup app file

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,26 @@
-import { app } from "./server";
+import express from "express";
+import cookiesParser from "cookie-parser";
 
 // Middlewares
 import errorHandler from "./middlewares/errorHandler.middleware.js";
 
+const app = express();
 
+app.use(cookiesParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+/* ---------------------------------------------------------------------------------------------- */
+
+// Routes
+import authRoutes from "./routes/auth.routes.js";
+
+// Use Routes
+app.use("/api/auth", authRoutes);
+
+/* ---------------------------------------------------------------------------------------------- */
+
+// Error Handler Middleware
 app.use(errorHandler);
+
+export default app;
