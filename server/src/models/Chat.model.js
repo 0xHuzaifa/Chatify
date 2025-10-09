706d84cbcd8d8d5934356f9dbf975235682e740a
0xhuzaifa <huzaifa.rb00@gmail.com>
2025-10-09 15:59:45 +0500

    set for group chats and add middlewares

--- server/src/models/Chat.model.js
+++ server/src/models/Chat.model.js
@@ -1,4 +1,5 @@
-import mongoose, { Schema } from "mongoose";
+import { Schema } from "mongoose";
+import getModelSafely from "../helpers/getModelSafely.js";
 
 const chatSchema = new Schema(
   {
@@ -10,21 +11,84 @@ const chatSchema = new Schema(
       },
     ],
 
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+
+    chatName: {
+      type: String,
+      trim: true,
+      required: function () {
+        return this.isGroupChat;
+      },
+    },
+
+    groupAdmin: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: function () {
+        return this.isGroupChat;
+      },
+    },
+
     lastMessage: {
       type: Schema.Types.ObjectId,
       ref: "Message",
     },
 
-    unreadCount: {
-      type: Number,
-      default: 0,
-    },
+    unreadCount: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        count: {
+          type: Number,
+          default: 0,
+          min: [0, "Unread message count cannot be negative"],
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
 
 chatSchema.index({ participants: 1 });
+chatSchema.index({ "unreadCount.user": 1 });
+chatSchema.index({ lastMessage: 1 });
 
-const Chat = mongoose.model("Chat", chatSchema);
+// Middleware: Pre-find - Populate latestMessage and participants
+chatSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "lastMessage",
+    select: "content contentType sender createdAt messageStatus",
+  }).populate({
+    path: "participants",
+    select: "fullName avatar isOnline",
+  });
+  next();
+});
 
+// Validation: Minimum 2 participants
+chatSchema.pre("validate", function (next) {
+  if (this.participants.length < 2) {
+    next(new ApiError(400, "Minimum 2 participants are required for chat"));
+  }
+
+  if (this.isNew) {
+    this.unreadCount = this.unreadCount.map((user) => ({
+      user: user,
+      count: 0,
+    }));
+  }
+  next();
+});
+
+// Method: Reset unread count for a user
+chatSchema.methods.resetUnreadCount = async function (userId) {
+  this.unreadCount = this.unreadCount.map((uc) =>
+    uc.user.toString() === uc.userId.toString() ? { ...uc, count: 0 } : uc
+  );
+  await this.save();
+};
+
+const Chat = getModelSafely("Chat", chatSchema);
 export default Chat;
