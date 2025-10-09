ccdc451f080c2e73ee821082d3417ac60a0368fc
0xhuzaifa <huzaifa.rb00@gmail.com>
2025-10-09 15:57:47 +0500

    protect routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -11,7 +11,7 @@ import {
   updateProfile,
   changePassword,
   getCurrentUser,
-  getAllUsers,
+  searchUsers,
 } from "../controllers/auth.controller.js";
 import authentication from "../middlewares/auth.middleware.js";
 
@@ -20,14 +20,16 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/verify", verifyUser);
 router.post("/login", login);
-router.post("/logout", logout);
 router.post("/resend-verification", resendVerification);
 router.post("/refresh-token", refreshAccessToken);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
-router.put("/profile", updateProfile);
+
+router.use(authentication);
+router.post("/logout", logout);
 router.post("/change-password", changePassword);
+router.put("/profile", updateProfile);
 router.get("/me", getCurrentUser);
-router.get("/users", authentication, getAllUsers);
+router.get("/users", searchUsers);
 
 export default router;
