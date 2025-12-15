f916eb7fdf0aab943431a3eb133348edc5ab8478
0xhuzaifa <huzaifa.rb00@gmail.com>
2025-12-15 14:09:47 +0500

    add provider

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 //@ts-ignore
 import "@/app/globals.css";
+import Providers from "./providers";
 
 const geist = Geist({ subsets: ["latin"] });
 const geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -26,7 +27,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geist.className} font-sans`}>{children}</body>
+      <body className={`${geist.className} font-sans`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
