e218ad1cf288283923eea2089db2ee8ea861d30d
0xhuzaifa <huzaifa.rb00@gmail.com>
2025-12-16 16:42:45 +0500

    add logout mutation

--- client/src/queries/auth.queries.ts
+++ client/src/queries/auth.queries.ts
@@ -1,6 +1,6 @@
 import { authApi } from "@/apis/auth.api";
 import { useAuthStore } from "@/store/auth.store";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 // --------------------- LOGIN ---------------------
 export const useLogin = () => {
@@ -11,6 +11,29 @@ export const useLogin = () => {
       const { user, accessToken } = res.data.data;
       useAuthStore.getState().setUser(user);
       useAuthStore.getState().setAccessToken(accessToken);
+      useAuthStore.getState().setIsAuthenticated(true);
+    },
+  });
+};
+
+// --------------------- LOGOUT ---------------------
+export const useLogout = () => {
+  return useMutation({
+    mutationFn: authApi.logout,
+
+    onSuccess: () => {
+      useAuthStore.getState().resetAuth();
+      useAuthStore.getState().triggerForceLogout();
+      document.cookie = `accessToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+      document.cookie = `refreshToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+    },
+
+    onError: () => {
+      // Even if logout API fails, clear local auth
+      useAuthStore.getState().resetAuth();
+      useAuthStore.getState().triggerForceLogout();
+      document.cookie = `accessToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+      document.cookie = `refreshToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
     },
   });
 };
