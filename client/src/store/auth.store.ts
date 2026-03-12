63c7ea0f467d3167b993a99cae7c71e857a351e9
0xhuzaifa <huzaifa.rb00@gmail.com>
2026-03-12 20:12:45 +0500

    modify auth store

--- client/src/store/auth.store.ts
+++ client/src/store/auth.store.ts
@@ -4,8 +4,15 @@ import { AuthState } from "@/types/auth.type";
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   accessToken: null,
+  isAuthenticated: false,
 
   setUser: (user) => set({ user }),
   setAccessToken: (token) => set({ accessToken: token }),
-  resetAuth: () => set({ user: null, accessToken: null }),
+  setIsAuthenticated: (status) => set({ isAuthenticated: status }),
+  triggerForceLogout: () => {
+    set({ user: null, accessToken: null, isAuthenticated: false });
+    // You can also emit an event or call a callback here if needed to redirect
+  },
+  resetAuth: () =>
+    set({ user: null, accessToken: null, isAuthenticated: false }),
 }));
