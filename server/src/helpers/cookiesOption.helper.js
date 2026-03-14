cbd918fdbc3e63de48840007e960f6fec16f0acb
0xhuzaifa <huzaifa.rb00@gmail.com>
2026-03-14 18:13:07 +0500

    modify cookies helper

--- server/src/helpers/cookiesOption.helper.js
+++ server/src/helpers/cookiesOption.helper.js
@@ -4,6 +4,6 @@ const generateCookieOptions = (maxAgeMs = 7 * 24 * 60 * 60 * 1000) => ({
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   maxAge: maxAgeMs,
   path: "/",
-  domain: "localhost",
+  ...(process.env.COOKIE_DOMAIN ? { domain: process.env.COOKIE_DOMAIN } : {}),
 });
 export default generateCookieOptions;
